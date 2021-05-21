cc6bebfe8cb1e813256a940a0c31553d24073ea8
Divakar Raut <divakar.raut@service.nsw.gov.au>
2021-05-21 17:18:46 +1000

    redux store updated

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './pages/login/loginSlice';
 
 const store = configureStore({
